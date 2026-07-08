84392671fbf6437702734c0b23cb2ca0008882a8
haris-coding <2025sse52@student.uet.edu.pk>
2026-07-08 14:42:22 +0500

    first commit

---
+++ lec2.js
@@ -0,0 +1,49 @@
+// console.log("helloword");
+//we can write comment using the double slash
+/*and for
+multiple line coment*/
+
+// expression a+b 
+// here a,b are operand and + is operator
+
+// arithmetic operators
+// let a=90;
+// let b=89;
+// console.log("a=90 , b=89")
+// console.log("a+b=",a+b);
+// console.log("a*b=",a*b);
+// console.log("a-b=",a-b);
+// //modulus operator
+// console.log("a%b=",a%b);
+// //exponent operator
+// console.log("a**b=",a**b);
+// //uniary operator
+// console.log("a++ = ",a++)
+// console.log("a-- = ",a--)
+
+//assignment operator
+//= , += , -= , 
+
+// let a=4;
+// let b=5;
+
+// a*=b;
+// console.log(a);
+
+//comparision operators
+//== , != , >= , <=
+// let a=5;
+// let b=6;
+// console.log(a==b);
+
+//logical operators
+// && ,|| , !
+// let a=5;
+// let b=6;
+// console.log(a>b && a===5)
+
+
+//ternary operator
+let age=18;
+let result=age>=18?"adult":"not adult";
+console.log(result);
